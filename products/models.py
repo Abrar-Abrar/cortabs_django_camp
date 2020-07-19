b03c3c69919c0f1f6719f3a4f69c961c00438428
Abrar <abrar_abdulwahed@yahoo.com>
2020-07-19 22:34:08 +0300

    add fileds and products by shell

--- products/models.py
+++ products/models.py
@@ -6,6 +6,8 @@ from django.urls import reverse
 class Product(models.Model):
     title = models.CharField(max_length=50, unique=True)
     description = models.CharField(max_length=250, blank=True,  null=True)
+    price = models.IntegerField(blank=True,  null=True)
+    create_at = models.DateField(blank=True,  null=True)
 
     def get_absolute_url(self):
         return reverse("product_details", kwargs={"pk": self.pk})
